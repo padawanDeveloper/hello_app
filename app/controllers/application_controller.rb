daba50e7dc5009a5844077494d2c980153c6d768
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-06 02:18:55 -0300

    Actualizacion gemfile Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "¡Hello, world!"
+  end
+
+  def goodbye
+    render html: "¡Chao, CTM!"
   end
 end
