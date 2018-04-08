7ee494699d5925f9d0d87a2fd8eea6eb0c8ee1e5
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-08 17:49:52 -0300

    validacion actualizada

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
 	belongs_to :user
-	validates :content, length: { maximum: 140 }
+	validates :content, presence: true, length: { maximum: 140 }
 	validates :user_id, presence: true
+	
 end
